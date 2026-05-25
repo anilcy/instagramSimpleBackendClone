5917b4858a843e58d871fd5c5c7c22634e681605
anilcy <forsocialm@outlook.com>
2026-05-25 18:14:01 +0200

    Comment entity updated

--- SocialMediaPlatform.Entities/Models/Comment.cs
+++ SocialMediaPlatform.Entities/Models/Comment.cs
@@ -1,4 +1,68 @@
+using System.Runtime.InteropServices.JavaScript;
+
 namespace SocialMediaPlatform.Entities.Models;
+
+public class Comment
+{
+    public Comment(Guid postId, Guid authorId, string content, Guid? parentCommentId = null)
+    {
+        // Guid id will be created automatically by database
+        if (postId == Guid.Empty)
+            throw new ArgumentException("Invalid post id.", nameof(postId));
+
+        if (authorId == Guid.Empty)
+            throw new ArgumentException("Invalid author id.", nameof(authorId));
+
+        if (string.IsNullOrWhiteSpace(content))
+            throw new ArgumentException("Content cannot be empty.", nameof(content));
+        
+        PostId = postId;
+        AuthorId = authorId;
+        Content = content;
+        ParentCommentId = parentCommentId;
+        LikeCount = 0; //??
+        CreatedAt = DateTimeOffset.UtcNow;
+        IsDeleted = false;
+    }
+
+    private Comment() { }
+    public Guid Id { get; private set; }
+    public Guid PostId { get; private set; }
+    public Guid AuthorId { get; private set; }
+    public string Content { get; private set; }
+    public Guid? ParentCommentId { get; private set; }
+    public int LikeCount { get; private set; }
+    public DateTimeOffset CreatedAt { get; private set; } 
+    public DateTimeOffset? UpdatedAt { get; private set; }
+    
+    //Status flag
+    public bool IsDeleted { get; private set; } 
+    
+    //Navigation Properties
+    public AppUser Author { get; set; } = null!;
+    public Post Post { get; set; } = null!;
+    public Comment? Parent { get; private set; }
+        
+    public ICollection<Comment> Replies { get; set; } = new List<Comment>();
+    public ICollection<CommentLike> Likes { get; set; } = new List<CommentLike>();
+    
+    public void UpdateContent(string newContent)
+    {
+        if(string.IsNullOrWhiteSpace(newContent))
+            throw new ArgumentException("Content cannot be empty.", nameof(newContent));
+        Content = newContent;
+        UpdatedAt = DateTimeOffset.UtcNow;
+    }
+    
+    public void SoftDelete()
+    {
+        IsDeleted = true;
+        UpdatedAt = DateTimeOffset.UtcNow;
+    }
+}
+
+
+/*namespace SocialMediaPlatform.Entities.Models;
 
 public class Comment
 {
@@ -19,4 +83,5 @@ public class Comment
         
     public ICollection<Comment> Replies { get; set; } = new List<Comment>();
     public ICollection<CommentLike> Likes { get; set; } = new List<CommentLike>();
-}
+}*/
+
